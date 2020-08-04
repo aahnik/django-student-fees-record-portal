e3fab1556c9200d082b4c233ff8d76d902a1fc63
Aahnik Daw <aahnikdaw@gmail.com>
2020-08-04 21:17:37 +0530

    made changes and then migrated succesfully

--- cdiig/student_management/admin.py
+++ cdiig/student_management/admin.py
@@ -1,7 +1,11 @@
-from cdiig.student_management.models import AcadTarget, Batch, Grade, Section, SessionCode, Student, StudyCenter
 from django.contrib import admin
-from .models import AcadType, AcadTarget, SessionCode,
-StudyCenter, Section, Grade, Batch, Student
+from .models import AcadType, AcadTarget, SessionCode, StudyCenter, Section, Grade, Batch, Student
 
-admin.site.register(AcadType, AcadTarget, SessionCode,
-                    StudyCenter, Section, Grade, Batch, Student)
+admin.site.register(AcadType)
+admin.site.register(AcadTarget)
+admin.site.register(SessionCode)
+admin.site.register(StudyCenter)
+admin.site.register(Section)
+admin.site.register(Grade)
+admin.site.register(Batch)
+admin.site.register(Student)
