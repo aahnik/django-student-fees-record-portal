8b5aede2bd6fe97068b5577f709a2d994cdef9c1
Aahnik Daw <aahnikdaw@gmail.com>
2020-08-05 01:14:21 +0530

    ran succesfully from mobile

--- cdiig/home/views.py
+++ cdiig/home/views.py
@@ -5,4 +5,4 @@ from django.utils import timezone
 def index(request):
     now = timezone.now()
     context = {'now': now}
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index', context)
