6945abf4f1b7e6e713ffe14e05a237f02db9d6b1
Aahnik Daw <aahnikdaw@gmail.com>
2020-08-07 00:07:08 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@ source venv_iig_admin/bin/activate
 cd cdiig/
 python3 manage.py makemigrations student_management
 python3 manage.py migrate
+python3 manage.py shell < fillStudentsSCRIPT.py
+python3 manage.py createsuperuser
 python3 manage.py runserver 192.168.1.8:8080
