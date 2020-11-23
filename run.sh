3fd7c6a8ed92398a0545d9ee4dcf88252e189b81
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-23 18:50:30 +0530

    delete this script

--- run.sh
+++
@@ -1,8 +0,0 @@
-# this is for my local dev environment not for server side
-source venv_iig_admin/bin/activate
-cd cdiig/
-python3 manage.py makemigrations student_management
-python3 manage.py migrate
-# python3 manage.py shell < fillStudentsSCRIPT.py
-# python3 manage.py createsuperuser
-python3 manage.py runserver 192.168.1.8:8080
